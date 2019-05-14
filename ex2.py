c4423b2b4ed22a4bad6dfd924007c350abc0248a
Eliyah Weinberg <eliyahweinberg@gmail.com>
2019-05-14 22:34:20 +0300

    adding implementations

--- ex2.py
+++ ex2.py
@@ -26,13 +26,13 @@ N = batch_size
 W = 28
 H = 28
 D = W * H
-B = 10
+num_classes = 10
 # [N=100,H,W,C=1]
-# correct labels
-y_ = tf.placeholder(tf.float32, shape=[H, N])  # YOUR CODE)
+
+y = tf.placeholder(tf.float32, shape=[N, num_classes])  # YOUR CODE)
 
 # input data
-x = tf.placeholder(tf.float32, shape=[H, D])  # YOUR CODE)
+x = tf.placeholder(tf.float32, shape=[N, D])  # YOUR CODE)
 
 """Next, define the network itself. It is up to you how many layers to use, and the number of hidden units in each layer.
 
@@ -49,38 +49,40 @@ Please note that each layer includes not only tf.matmul, but also a bias variabl
 # build the net
 
 # hidden_size =  # YOUR CODE
-num_classes = 2
-W_fc1 = weight_variable([D, N])
-b_fc1 = bias_variable([1,10])
-h_fc1 = tf.nn.relu(tf.matmul(x, W_fc1) + b_fc1) # YOUR CODE
+#num_classes = 2
+W_fc1 = weight_variable([D, num_classes])
+b_fc1 = bias_variable([num_classes])
+h_fc1 = tf.nn.relu(tf.add(tf.matmul(x, W_fc1), b_fc1)) # YOUR CODE
 
-W_fc2 = weight_variable([N, D])
-b_fc2 = bias_variable([10,1])
-h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2) # YOUR CODE
+W_fc2 = weight_variable([num_classes, D]) #10x784
+b_fc2 = bias_variable([D])
+h_fc2 = tf.nn.relu(tf.add(tf.matmul(h_fc1, W_fc2), b_fc2)) # 100x784
 
-# YOUR CODE (how many layers to use, and the number of hidden units)
+W_fc3 = weight_variable([D, num_classes])
+b_fc3 = bias_variable([num_classes])
+logits = tf.nn.relu(tf.add(tf.matmul(h_fc2, W_fc3), b_fc3))
 
-# y = tf.nn.softmax()  # YOUR CODE )
-#
-# """Complete the snippet below using your own code.
-#
-# define the loss function and Optimizer
-# """
+hypothesis = tf.nn.softmax(logits)
 
-# define the loss function
-# cross_entropy =  # YOUR CODE
-#
-# # define Optimizer
-# Optimizer = tf.train  # YOUR CODE (cross_entropy)
-#
-#
-# correct_prediction =  tf.placeholder # YOUR CODE
-# accuracy =  true_prediction/N (num of samples in Batch Size
-# init = tf.global_variables_initializer()
-#
-# """The next code snippet trains and evaluates the network. It does this by opening a session to run the tensorflow graph that we have defined.
-# Complete the code at the locations marked #YOUR CODE below, in order to train the network and to evaluate its accuracy every 50 steps.
-# """
+"""Complete the snippet below using your own code.
+
+define the loss function and Optimizer
+"""
+
+#define the loss function
+cross_entropy =  # YOUR CODE
+
+# define Optimizer
+Optimizer = tf.train  # YOUR CODE (cross_entropy)
+
+
+correct_prediction =  tf.placeholder # YOUR CODE
+accuracy =  #true_prediction/N (num of samples in Batch Size
+init = tf.global_variables_initializer()
+
+"""The next code snippet trains and evaluates the network. It does this by opening a session to run the tensorflow graph that we have defined.
+Complete the code at the locations marked #YOUR CODE below, in order to train the network and to evaluate its accuracy every 50 steps.
+"""
 
 with tf.Session() as sess:
     sess.run(init)
@@ -89,7 +91,7 @@ for i in range(700):
 
     # YOUR CODE
     train_step = tf.Variable()
-    sess.run(train_step, feed_dict={X: input_images, correct_predictions:correct_predictions })# YOUR CODE
+    sess.run(train_step, feed_dict={x: input_images, y:correct_predictions})# YOUR CODE
     if i % 50 == 0:
         train_accuracy = sess.run(accuracy, feed_dict={  # YOUR CODE } )
         print("step %d, training accuracy %g" % (i, train_accuracy))
